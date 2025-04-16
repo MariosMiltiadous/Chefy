a1c776b6f0162b5f2453283f9e619ee06858f03d
Marios <46002841+MariosMiltiadous@users.noreply.github.com>
2025-04-16 18:56:49 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Main from "./components/Main";
+import Header from "./navigation/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+}
+
+export default App;
---
+++ src/navigation/Header.jsx
@@ -0,0 +1,12 @@
+import chefLogo from '../assets/chefy-logo.png'
+
+export default function Header() {
+
+    return (
+    <header>
+        <img src={chefLogo} alt="logo"/>
+        <h1>Chef Claude</h1>
+    </header>
+  )
+  }
+  
