b1f2965b8d268dc37b5e20d2c25027650d4375c4
Marios Miltiadous (CY) <marios.miltiadous@pwc.com>
2025-04-21 21:05:00 +0300

    Fix index styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,29 @@
 export default function Main() {
+
+  const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    const newIngredient = formData.get("ingredient")
+    alert(newIngredient)
+  }
+
   return (
     <main>
-      <form className="add-ingredient-form">
-        <input type="text" placeholder="" aria-label="Add ingredient" />
+      <form className="add-ingredient-form" onSubmit={handleSubmit}>
+        <input type="text"
+          placeholder="e.g. oregano"
+          aria-label="Add ingredient"
+          name="ingredient"
+        />
         <button>Add ingredient</button>
       </form>
+      <ul className="ingredients-list">
+        {ingredients.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </main>
   );
 }
