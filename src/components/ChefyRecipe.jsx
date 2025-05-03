bee025615dea016ec2664243dea1c34a8fd8b680
Marios <46002841+MariosMiltiadous@users.noreply.github.com>
2025-05-03 09:36:31 +0300

    Add please text on chefy recipe

--- src/components/ChefyRecipe.jsx
+++ src/components/ChefyRecipe.jsx
@@ -9,6 +9,7 @@ export default function ChefyRecipe({ recipe, loadRecipe }) {
   return (
     <section className="suggested-recipe-container" aria-live="polite">
       <h2>Chefy Recommends:</h2>
+      {!recipe && <p>Please add at least 4 ingredients</p>}
       <ReactMarkdown>{recipe}</ReactMarkdown>
     </section>
   );
